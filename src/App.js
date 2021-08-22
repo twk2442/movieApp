8bb9797bb89a7c4162496e4ab384d67eb494980f
twk2442 <twk2442@gmail.com>
2021-08-22 19:00:34 +0900

    react:jsx+props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food(props) {
+  // props 대신 {fav} 사용가능
+  // component의 이름 첫글자는 대문자로 시작해야함
+  return <h1>i like {props.fav}</h1>; // {props.fav}와 {fav}는 같다
+}
 
 function App() {
   // App이라는 컴포넌트 컴포넌트는 html을 반환하는 함수
   return (
     <div className="App">
       hello!!
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
