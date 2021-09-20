5fda3891065abe3812c2eac877e5bdb7ce6e5542
twk2442 <twk2442@gmail.com>
2021-09-20 23:27:07 +0900

    map

--- src/movie.js
+++ src/movie.js
@@ -11,11 +11,16 @@ function Movie({ id, year, title, summary, poster, genres }) {
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <ul className="genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
+          {genres.map(
+            (
+              genre,
+              index // genre매핑
+            ) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            )
+          )}
         </ul>
         <p className="movie__summary">{summary}</p>
       </div>
