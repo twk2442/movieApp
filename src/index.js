1e389f2bb8d781c30c3b1cdf8932ef874a01e2df
twk2442 <twk2442@gmail.com>
2021-10-09 23:09:33 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // App은 컴포넌트  리엑트는 컴포넌트로 동작한다
 // component 는 html을 반환하는 함수
 // <컴포넌트/> 같은 문법은 jsx라는 개념
+// redux 사용이유: 1. props사용없이 state 사용가능 2. state관리 용이
